03cb616d1b16a95886cfb9e02d305335d8b74a54
Hatice <hatice.karabacak@ing.com>
2023-10-19 16:13:20 +0200

    New files added

---
+++ src/main/java/umfrage/Application/Umfrage/UmfrageApplication.java
@@ -0,0 +1,13 @@
+package umfrage.Application.Umfrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmfrageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmfrageApplication.class, args);
+	}
+
+}
