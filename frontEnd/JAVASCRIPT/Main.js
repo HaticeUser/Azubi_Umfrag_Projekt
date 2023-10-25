c677e0924dedfbfbfb24530818e19bc56d1ae14a
Hatice <hatice.karabacak@ing.com>
2023-10-25 11:23:03 +0200

    Update

--- frontEnd/JAVASCRIPT/Main.js
+++ frontEnd/JAVASCRIPT/Main.js
@@ -1,11 +1,14 @@
     "use strict"
 
     import {locationsHandler} from  "./Locationhandler.js"
-
+    import {Opinion_Poll} from "./Opinion_Poll.js"
 
     let locationHref = new locationsHandler();
+    let opinion_Poll = new Opinion_Poll();
 
-    function handleRequest(){
+   export function handleRequest(){
+
+        opinion_Poll.createInput();
 
     }
    export function handleLocationID(locationID){
--- frontEnd/JAVASCRIPT/Opinion_Poll.js
+++ frontEnd/JAVASCRIPT/Opinion_Poll.js
@@ -1,14 +1,35 @@
 export class Opinion_Poll{
     constructor() {
-        this.input_ID = 0;
+        this.input_ID = 1.12;
+        this.savedInput_ID=0;
         this.createdInputLength=0;
-
+        this.createdInput_Container = document.getElementById("createdInput-Container")
+        this.input_object = JSON.parse(localStorage.getItem("input_Data")) || {
+            input_Value: String,
+            input_ID:Number,
+                inputTheme:{
+                 inputTheme_Value:String,
+                 input_Theme_ID: Number,
+            }
+        }
     }
 
     createInput(){
-        let createdInput= document.createElement("input");
+        try {
+            let createdInput= document.createElement("input");
+            createdInput.classList.add("opinionInput");
+            this.savedInput_ID= createdInput.id=this.input_ID;
+            this.createdInput_Container.append(createdInput);
 
+            localStorage.setItem("input_Data",JSON.stringify(this.savedInput_ID))
 
+            this.createdInputLength++;
+            this.input_ID +=1.12;
+            console.log(this.input_ID);
+            console.log(this.createdInputLength);
+        }catch(err){
+            throw "creating an input didnt work"
+        }
     }
 
 }
