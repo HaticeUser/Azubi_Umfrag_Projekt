d420baea55c0c9a92ffadcc0702d3783cdba82d0
Hatice <hatice.karabacak@ing.com>
2023-10-25 14:41:50 +0200

    Updated

--- frontEnd/JAVASCRIPT/Opinion_Poll.js
+++ frontEnd/JAVASCRIPT/Opinion_Poll.js
@@ -4,14 +4,19 @@ export class OpinionPoll{
         this.savedInput_ID=0;
         this.createdInputLength=0;
         this.createdInput_Container = document.getElementById("createdInput-Container")
-        this.input_object = JSON.parse(localStorage.getItem("input_Data")) || {
+        this.input_Data_Info = [
+            {
             input_Value: String,
             input_ID:Number,
+            createdInput_Length:Number,
             inputTheme:{
                 inputTheme_Value:String,
                 input_Theme_ID: Number,
             }
         }
+        ]
+
+        this.createdOpinion_Polls=0;
     }
 
     createInput( ){
@@ -22,12 +27,23 @@ export class OpinionPoll{
             this.createdInput_Container.append(createdInput);
              this.savedInput_ID= createdInput.id=this.input_ID;
 
-             localStorage.setItem("input_Data",JSON.stringify(this.savedInput_ID))
+             this.saveData()
 
              this.createdInputLength++;
              this.input_ID +=1.12;
-             console.log(this.input_ID);
+             console.log(this.savedInput_ID);
              console.log(this.createdInputLength);
+    }
+
+
+    saveData(){
+
+        this.input_Data_Info[0].input_Value();
+        this.input_Data_Info[0].input_ID();
+        this.input_Data_Info[0].createdInput_Length();
+        this.input_Data_Info[0].inputTheme.input_Theme_ID();
+        this.input_Data_Info[0].inputTheme.inputTheme_Value();
+
 
 
     }
