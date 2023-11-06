9b0b447dfdcff3856c7acb7e20ee472380808cd5
Hatice <hatice.karabacak@ing.com>
2023-11-06 14:26:12 +0100

    design new Sister Site

---
+++ frontEnd/JAVASCRIPT/Opinion_Oracle.js
@@ -0,0 +1,17 @@
+/*
+
+*Modell Design und html anfragen bearbeiten,
+*front end logik ausbauen (getrennt vom html).
+
+*
+
+Bei der Logik ist es so
+Wenn wir auf Opinion_Poll eine umfrage erschaffen, dann posten, wird alles inm backend gespeichert und an eine
+datenbank übergeben, gleichzeitig verarbeitet das backend die daten und gibt sie im frontend
+auf dieser seite aus.
+
+Also müssen wir hier eine get anfrage posten.
+
+*
+ */
+
